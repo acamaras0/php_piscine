b3bb5b2c371fbd25a0679819e300294e58aa4032
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-14 19:01:30 +0300

    ex03

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -4,8 +4,6 @@ date_default_timezone_set('Europe/Helsinki');
 $f = fopen("/var/run/utmpx", "r");
 while ($fd = fread($f, 628))
     {
-        if (strlen($fd) == 628)
-        {
             $fd = unpack("a256user/a4id/a32tty/ipid/itype/itime", $fd);
             if ($fd['type'] == 7)
             {
@@ -16,7 +14,6 @@ while ($fd = fread($f, 628))
                 $time = date("H:i", $fd['time']);
                 printf("%-8s %-8s %s %2s %s\n", $usr, $t, $month, $day, $time);
             }
-        }
     }
-    fclose($f);
+fclose($f);
 ?>
