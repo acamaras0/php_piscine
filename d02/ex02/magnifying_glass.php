c43cc954d822ef5ca9377719dd98a55c7dd16aa6
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-14 20:01:15 +0300

    stuff

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -1,29 +1,16 @@
 #!/usr/bin/php
 <?php
-
-	function ft_replace_pattern1($matches)
-	{	
-		return ('title='.strtoupper($matches[1]));
-	}
-	function ft_replace_pattern2($matches)
-	{
-		return (">".strtoupper($matches[1])."<");
-	}
-	function ft_replace($matches)
-	{
-		$str = preg_replace_callback('/title=(\s*["\'][^"\']*)/', 'ft_replace_pattern1', $matches[1]);
-		if (preg_match("/<.*/", $matches[2]))
-		{
-			$tmp = preg_replace_callback('/title=(\s*["\'][^"\']*)/', 'ft_replace_pattern1', $matches[2]);
-			$tmp = preg_replace_callback("/>([^<]*)</", 'ft_replace_pattern2', $tmp);
-			$str = $str.$tmp.$matches[3];
-		}
-		else
-			$str = $str.strtoupper($matches[2]).$matches[3];
-		return ($str);
-	}
-	if ($argc == 1)
-		return ;
-	$str = file_get_contents($argv[1]);
-	echo preg_replace_callback('/(<a [^>]*)(>.*?)(<\/a>)/sim', 'ft_replace', $str);
+    $pattern0 = "/(?<=)<a.*?<\/a>/ism";
+    $pattern1 = "/(?<=>).*?<|(?<=title)=\"(.*?)\"/ism";
+    if ($argc == 2)
+    {
+        $str = implode("", file($argv[1]));
+        $ret = $str;
+        preg_match_all($pattern0, $str, $match);
+        $str = implode("", $match[0]);
+        preg_match_all($pattern1, $str, $match);
+        foreach($match[0] as $string)
+            $ret = preg_replace("/$string/", strtoupper($string), $ret);
+        echo $ret;
+    }
 ?>
