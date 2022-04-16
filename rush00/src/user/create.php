f3b419b47630c6f6dfa78abb1230f3173fa876bd
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-16 13:36:18 +0300

    create user

---
+++ rush00/src/user/create.php
@@ -0,0 +1,35 @@
+<?php
+    if($_POST["login"] && $_POST["passwd"] &&
+       $_POST["submit"] && $_POST["submit"] == "OK")
+    {
+        if(file_exists("../private") == false)
+            mkdir("../private");
+        if(file_exists("../private/passwd") == false)
+            file_put_contents("../private/passwd", NULL);
+        $account = unserialize(file_get_contents('../private/passwd'));
+        if($account)
+        {
+            $exists = 0;
+            foreach($account as $key => $value)
+            {
+                if ($value["login"] === $_POST["login"])
+                {
+                    echo "Account already exists.\n";
+                    $exists = 1;
+                }
+            }
+        }
+        if (!$exists)
+        {
+            $temp["login"] = $_POST["login"];
+            $temp["passwd"] = hash('whirlpool', $_POST["passwd"]);
+            $account[] = $temp;
+            file_put_contents('../private/passwd', serialize($account));
+            echo "Account created!\n";
+        }
+        else
+            echo "ERROR: Try again!\n";
+    }
+    else
+        echo "ERROR\n";
+?>
