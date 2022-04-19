f349fcb9157fccfe03b1ca70bda11d015b6f730f
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-19 14:29:51 +0300

    changed

--- d05/ex18/ex18.sql
+++ d05/ex18/ex18.sql
@@ -1,5 +1,6 @@
 SELECT `name`
 FROM `distrib`
 WHERE (`id_distrib` IN (42, 62, 63, 64, 65, 66, 67, 68, 69, 71, 88, 89, 90)) OR
-      (LOWER(`name`) LIKE '%y%y%')
+      (LOWER(`name`) LIKE '%y%y%') AND
+      (LOWER(`name`) NOT LIKE '%y%y%y%')
 LIMIT 2, 5;
