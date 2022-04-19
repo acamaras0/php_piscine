07d52c7ba8bb1530be0fe9b5e9c032d01ee498c9
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-19 21:55:32 +0300

    final

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -1,4 +1,4 @@
-SELECT UPPER(`last_name`) AS `NAME`, `first_name`, `price`
+SELECT UPPER(`last_name`) AS 'NAME', `first_name`, `price`
 FROM `member`
 JOIN `user_card` ON member.id_user_card = user_card.id_user
 JOIN `subscription` ON member.id_sub = subscription.id_sub
--- d05/ex18/ex18.sql
+++ d05/ex18/ex18.sql
@@ -1,6 +1,5 @@
 SELECT `name`
 FROM `distrib`
 WHERE (`id_distrib` IN (42, 62, 63, 64, 65, 66, 67, 68, 69, 71, 88, 89, 90)) OR
-      (LOWER(`name`) LIKE '%y%y%') AND
-      (LOWER(`name`) NOT LIKE '%y%y%y%')
+      (LENGTH(name) - LENGTH(REPLACE(LOWER(name), 'y', '')) = 2)
 LIMIT 2, 5;
