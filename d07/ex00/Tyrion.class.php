3e076884ed0181c40577902372b5673fd3910b5f
Anamaria Camarasan <acamaras@student.hive.fi>
2022-04-21 20:13:58 +0300

    fix

--- d07/ex00/Tyrion.class.php
+++ d07/ex00/Tyrion.class.php
@@ -1,9 +1,14 @@
 <?php
     class Tyrion extends Lannister
     {
+        public function __construct()
+        {
+            parent::__construct();
+            printf("My name is Tyrion\n");
+        }
         public function getSize()
         {
-            printf("My name is Tyrion\nShort");
+            printf("Short");
         }
     }
 ?>
